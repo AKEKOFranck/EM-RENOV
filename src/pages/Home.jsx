662b25079153c7d95f701bc52b67a0787f4d67a3
AKEKOFranck <142504737+MauriceAkeko@users.noreply.github.com>
2025-10-23 19:31:08 +0000

    new test 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,18 @@
 import '../styles/Home.css';
 import { Link } from 'react-router-dom';
 
-
 export default function Home() {
-   
     return(
-        
         <section className='home_main'>
-          
             <h3 className='home_text'>
-                   L'art de la rénovation, l'exigence du détail
+                L'art de la rénovation, l'exigence du détail
             </h3>
-            <button type='button' className='home_bt'>
-                    <a href='/Work' style={{textDecoration: 'none'}}>
-                      Explorer nos oeuvres
-                    </a>
-            </button>
+            
+            <Link to="/Work" className='home_bt_link'>
+                <button type='button' className='home_bt'>
+                    Explorer nos oeuvres
+                </button>
+            </Link>
         </section>
     )
 }
